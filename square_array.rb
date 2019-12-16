789efaac75d0fc547667ba260d2a3814e36000a4
Lydia B <lydiabaik@gmail.com>
2019-12-16 03:10:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
-def square_array(array)
-  new_array = []
-  array.each{|number| new_array << number ** 2}
-  new_array
-end
+# def square_array(array)
+#   new_array = []
+#   array.each{|number| new_array << number ** 2}
+#   new_array
+# end
 
-
-array.collect{|number| number ** 2}
+def square_array_collect(array)
+  array.collect{|number| number ** 2}
+  
