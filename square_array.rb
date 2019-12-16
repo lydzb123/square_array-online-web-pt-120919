23e08aee36c1d3ea2f09ea3670830d862153dc80
Lydia B <lydiabaik@gmail.com>
2019-12-16 03:05:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
 def square_array(array)
-  new_array = [array.each do |number| number ** 2
-  end]
-  return new_array
+  new_array = []
+  array.each do |number| new_arary << number ** 2
+  end
+  new_array
 end
